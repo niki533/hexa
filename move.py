6bf96bb5f1760bc494250eeb58a75d886d3f5622
niki533 <106265711+niki533@users.noreply.github.com>
2024-05-25 16:23:26 +0545

    Update move.py

--- move.py
+++ move.py
@@ -1,48 +1,46 @@
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
+from __future__ import division
+import time
+
+# Import the PCA9685 module.
 import Adafruit_PCA9685
-import time  
 
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(50)
 
-init_pwm0 = 300
-init_pwm1 = 300
-init_pwm2 = 300
-init_pwm3 = 300
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
-init_pwm4 = 300
-init_pwm5 = 300
-init_pwm6 = 300
-init_pwm7 = 300
+# Configure min and max servo pulse lengths
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
 
-init_pwm8 = 300
-init_pwm9 = 300
-init_pwm10 = 300
-init_pwm11 = 300
+# Helper function to make setting a servo pulse width simpler.
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
 
-init_pwm12 = 300
-init_pwm13 = 300
-init_pwm14 = 300
-init_pwm15 = 300
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
 
-def singleServo(self, ID, direcInput, speedSet):
-		self.wiggleID = ID
-		self.wiggleDirection = direcInput
-		self.scSpeed[ID] = speedSet
-		self.scMode = 'wiggle'
-		self.posUpdate()
-		self.resume()
-
-sc = singleServo()   # Instantiate the object that controls the servo
-sc.start()  # Start this thread, when the servo does not move, the thread is suspended
-
-def stopWiggle(self):
-		self.pause()
-		self.posUpdate()
-
-while 1:  
-	sc.singleServo(3, -1, 2)      
-	time.sleep(1)  
-	sc.stopWiggle()     
-	sc.singleServo(3, 1, 2)  
-	time.sleep(1)  
-	sc.stopWiggle()  
+print('Moving servo on channel 0, press Ctrl-C to quit...')
+while True:
+    # Move servo on channel O between extremes.
+    pwm.set_pwm(0, 0, servo_min)
+    time.sleep(1)
+    pwm.set_pwm(0, 0, servo_max)
+    time.sleep(1)
